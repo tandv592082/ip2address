6aff7ba5ac1ff26106f49b056fbca8865c5ee3ad
TanDv <tandv592082@gmail.com>
2023-06-28 15:21:54 +0700

    chore: add download to excel

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const analysis = async () => {
     $(`small[id^="error-"]`).hide();
     $(`#table`).hide();
-    
+    $('#export2excel').hide();
+
 
     const errorMgs = {};
     const files = $('#file_upload')[0].files;
     const sheetName = $('#sheet-name').val();
     const rowName = $('#row-ip-name').val();
     let filename;
-    
-    if(!files.length) {
+
+    if (!files.length) {
         errorMgs['file_upload'] = 'Please select a file!';
     } else {
         filename = files[0].name;
@@ -20,18 +21,18 @@ const analysis = async () => {
         }
     }
 
-    if(!sheetName) {
+    if (!sheetName) {
         errorMgs['sheet-name'] = 'Please enter sheet name!'
     }
 
-    if(!rowName) {
+    if (!rowName) {
         errorMgs['row-ip-name'] = 'Please enter ip row name!';
     }
 
     // check valid form
-    const invalidForm = Object.keys(errorMgs);
+    const invalidForm = Object.keys(errorMgs);
 
-    if(invalidForm.length) {
+    if (invalidForm.length) {
         Object.entries(errorMgs).map((pair) => {
             $(`#error-${pair[0]}`).show().text(pair[1]);
         })
@@ -46,29 +47,29 @@ const analysis = async () => {
         const reader = new FileReader();
         reader.readAsBinaryString(files[0]);
 
-        reader.onload = async function(e) {
+        reader.onload = async function (e) {
             disableLayout();
             const data = e.target.result;
             const workbook = XLSX.read(data, {
-                type : 'binary'
+                type: 'binary'
             });
-            
+
             workbook.SheetNames.map((sheetName) => {
                 const roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
                 if (roa.length > 0) {
                     excel[sheetName] = roa;
                 }
             });
- 
+
             let sheet = excel[sheetName];
 
-            if(!sheet) {
+            if (!sheet) {
                 $(`#error-sheet-name`).show().text(`Sheet ${sheetName} does not exist or contain empty row!`);
                 enableLayout();
                 return;
             }
 
-            if(!sheet.length) {
+            if (!sheet.length) {
                 $(`#error-sheet-name`).show().text(`Sheet ${sheetName} not contain any row name!`);
                 enableLayout();
                 return;
@@ -76,22 +77,25 @@ const analysis = async () => {
 
             const ips = sheet.map(row => row[rowName]);
 
-            if(ips.every(k => k === undefined)) {
+            if (ips.every(k => k === undefined)) {
                 $(`#error-row-ip-name`).show().text(`Row ${rowName} does not exist!`);
                 enableLayout();
                 return;
             }
 
-            
             const promises = ips.map(getAddressFromIp);
             const result = await Promise.all(promises);
 
             enableLayout();
             renderTable(result);
-            
+            $('#export2excel').show().click((e) => {
+                e.preventDefault();
+                downloadXLS(result);
+            })
+
         }
 
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
 }
@@ -99,12 +103,12 @@ const analysis = async () => {
 
 const renderTable = (data) => {
     $('#table').dataTable().fnDestroy();
-    
     $('#table').show().DataTable({
         data,
         columns: [
             { data: 'ip' },
-            { data: 'address' },
+            { data: 'city' },
+            { data: 'country' },
         ],
         "pageLength": 10
     });
@@ -114,24 +118,67 @@ const getAddressFromIp = async (ip) => {
     console.log(ip);
     const uri = `http://ip-api.com/json/${ip}`;
     const response = await fetch(uri);
-    let address = 'N/A';
+    let data  = {};
+    
 
-    if(response.ok) {
-        const data = await response.json();
-        address = data.regionName ?? 'N/A';
+    if (response.ok) {
+         data = await response.json();
+        
     }
 
+    const {country, city} = data;
     return {
         ip,
-        address
+        city: city ?? 'N/A',
+        country: country ?? 'N/A',
     };
-   
+
 }
 
 const disableLayout = () => {
     $(`#loading`).show();
     $('input').attr('disabled', true);
     $('button').attr('disabled', true);
+}
+
+const downloadXLS = (xlsRows) => {
+    $("#table-download-body").empty();   
+    const createXLSLFormatObj = [];
+
+    /* XLS Head Columns */
+    const xlsHeader = ["IP", "City", 'Country'];
+    createXLSLFormatObj.push(xlsHeader);
+
+    $.each(xlsRows, function (index, value) {
+        const innerRowData = [];
+        $("#table-download-body").append('<tr><td>' + value.ip + '</td><td>' + value.city  + '</td><td>' + value.country + '</td></tr>');
+
+        $.each(value, function (ind, val) {
+
+            innerRowData.push(val);
+        });
+        createXLSLFormatObj.push(innerRowData);
+    });
+
+
+    /* File Name */
+    const filename = "IP_TO_LOCATION.xlsx";
+
+    /* Sheet Name */
+    const ws_name = "IP TO LOCATION";
+
+    if (typeof console !== 'undefined') console.log(new Date());
+    const wb = XLSX.utils.book_new(),
+        ws = XLSX.utils.aoa_to_sheet(createXLSLFormatObj);
+
+    /* Add worksheet to workbook */
+    XLSX.utils.book_append_sheet(wb, ws, ws_name);
+
+    /* Write workbook and Download */
+    if (typeof console !== 'undefined') console.log(new Date());
+    XLSX.writeFile(wb, filename);
+    if (typeof console !== 'undefined') console.log(new Date());
+
 }
 
 
@@ -154,15 +201,15 @@ $('.close-modal').click((e) => {
 })
 
 
-$('span[id^=explain-').click(function() {
-    switch($(this).attr('id').replace('explain-', '')) {
+$('span[id^=explain-').click(function () {
+    switch ($(this).attr('id').replace('explain-', '')) {
         case 'row-ip-name':
             $('.modal-title').text('IP row name');
-            $(".image-explain").attr("src","./assets/images/row-ip-name.png");
+            $(".image-explain").attr("src", "./assets/images/row-ip-name.png");
             break;
         case 'sheet-name':
             $('.modal-title').text('Sheet name');
-            $(".image-explain").attr("src","./assets/images/sheet-name.png");
+            $(".image-explain").attr("src", "./assets/images/sheet-name.png");
             break;
         default:
             break;
